7bcdcd2639d6aab7fe1daa86cc763600583d46d0
AlbertKhayrullin <albertKh1620@rambler.ru>
2021-03-10 10:06:22 +0300

    fixed#1

--- src/main/java/tech/itpark/domain/Film.java
+++ src/main/java/tech/itpark/domain/Film.java
@@ -8,10 +8,4 @@ public class Film {
     private long id;
     private String name;
     private double rating;
-
-    public Film(int id, String name, double rating) {
-        this.id = id;
-        this.name = name;
-        this.rating = rating;
-    }
 }
